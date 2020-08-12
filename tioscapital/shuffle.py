c45f59133ca19d2e3a74835f2d972debe1a1915f
cspradli <cspradli@unca.edu>
2020-08-12 09:28:02 -0400

    added updates

--- tioscapital/shuffle.py
+++ tioscapital/shuffle.py
@@ -9,17 +9,17 @@ def shuffle(amount, total_amount, deck, flag1, flag2):
     for card in range(len(bottom_half)):
         new_deck.append(top_half[card])
         new_deck.append(bottom_half[card])
-    print("%d / %d" % (total_amount - amount, total_amount))
+    print("%d / %d" % (total_amount - amount+1, total_amount))
     print(new_deck)
     if(flag2):
         ## Flag 2 represents to look for top and bottom cards touching problem ##
         for card in range(52):
             if new_deck[card] == 52 and (new_deck[card-1] == 1 or new_deck[card+1] == 1):
-                print("Top and bottom are touching on shuffle number %d" % (total_amount - amount))
+                print("Top and bottom are touching on shuffle number %d" % (total_amount+1 - amount))
                 return new_deck
     if(flag1 and new_deck[0] == 52):
         ## Flag 1 represents to look for 52 on bottom ##
-        print("52 on bottom at shuffle number %d" % (total_amount - amount))
+        print("52 on bottom at shuffle number %d" % (total_amount+1 - amount))
         return new_deck
     shuffle(amount-1, total_amount, new_deck, flag1, flag2)
 
@@ -33,11 +33,8 @@ set_number = 1
 for _ in range(52):
     deck.append(set_number)
     set_number += 1
-
-print(deck)
-shuffle(1, 1, deck, 0, 0)
 ##What is the position of the first card after the 7th shuffle? ##
-print("7th")
+print("Look for first card on 7th shuffle")
 shuffle(7, 7, deck, 0, 0)
 ## How many times must one perform the shuffle so that the top card is the bottom card ##
 shuffle(50, 50, deck, 1, 0)
